0009cc1e00e6465eec772baea386c7486d0044d7
EUMC1ST <edgar.morales@softtek.com>
2020-07-27 14:56:11 -0500

    Updating CRUD

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,28 @@
 const Product = require('../models/product');
 const product = require('../models/product');
+
 module.exports = {
-    show:async (req,res)=>{
+    create: (req,res)=>{
+        console.log(req.body);
+        let product = new Product();    
+        product.name = req.body.name;
+        product.picture = req.body.picture;
+        product.price = req.body.price;
+        product.category = req.body.category;
+        product.description = req.body.description;
+        product.save((err,product)=>{
+            if(err) res.status(500).send({message:'Error al guardarlo en la base de datos',errorType:err});
+            res.status(200).send({product});
+        })
+    },
+    showAll: (req,res)=>{
+        Product.find((err,products)=>{
+            if(err)return res.status(500).send({message:`Error al realizar la peticion ${error}`})
+            if(!products) retun.status(200).send({message:`There are not products`})
+            res.status(200).send({products})
+        }) //check if return a promise
+    },
+    showById:async (req,res)=>{
         let productId = req.params.id;
         //Using Promises
         // Product.findById(productId).exec().then((result)=>{
@@ -28,20 +49,20 @@ module.exports = {
         //     res.status(200).send(product);
         // });
     },
-    create: (req,res)=>{
-            console.log(req.body);
-            let product = new Product();    
-            product.name = req.body.name;
-            product.picture = req.body.picture;
-            product.price = req.body.price;
-            product.category = req.body.category;
-            product.description = req.body.description;
-            product.save((err,product)=>{
-                if(err) res.status(500).send({message:'Error al guardarlo en la base de datos',errorType:err});
-                res.status(200).send({product});
-            })
+    edit:(req,res)=>{
+        let _id = req.params.id;
+        Product.updateOne({_id},{name:req.body.name},(error,result)=>{
+            if(error) return res.status(500).send({message:`Error al realizar la peticion`});
+            if(!result) return res.status(400).send({message:`Product doesn't exist`});
+            return res.status(200).send({message:`Product updated succesfully`,result});
+        })
     },
-    destroy:()=>{
-
+    destroy:(req,res)=>{
+        let productId = req.params.id;
+        Product.findByIdAndDelete(productId,(err,product)=>{
+            if(err) return res.status(500).send({message:`Error al realizar la peticion ${err}`});
+            if(!product) return res.status(400).send({message:`El producto no fue eliminado`})
+            res.status(200).send({message:`Producto ${product.name} eliminado con exito`,product})
+        })
     }
 }
--- routes/products_routes.js
+++ routes/products_routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const productController = require('../controllers/products');
 let router = express.Router();
 
-router.route('/products').post(productController.create);
-router.route('/products/:id').get(productController.show);
+router.route('/products').post(productController.create).get(productController.showAll);
+router.route('/products/:id').get(productController.showById).delete(productController.destroy).put(productController.edit);
 
 
 module.exports = router;
